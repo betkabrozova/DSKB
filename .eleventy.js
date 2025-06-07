61ebf9e1cfff0b4d4bd650315ad7f1045b4acc70
betka brozova <brozova.b@gmail.com>
2025-06-07 13:34:06 +0200

    odkazy

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,14 @@
-export default async function(eleventyConfig) {
-  // Výchozí výstupní složka je: _site
+module.exports = function(eleventyConfig) {
   // Zkopírovat složku images/ do _site/images
   eleventyConfig.addPassthroughCopy("images");
-  // Zkopírovat složku css/ to _site/css/
+  // Zkopírovat složku css/ do _site/css
   eleventyConfig.addPassthroughCopy("css");
-}
+  eleventyConfig.addPassthroughCopy("pdf");
 
-export const config = {
-  // jako šablonu nebo soubor s obsahem
-  // můžeme použít následující formáty
-  templateFormats: ["njk", "html", "md"],
-
-  // jako šablonovací jazyk zvolíme pro všechny
-  // formáty výše jazyk Nunjucks
-  markdownTemplateEngine: "njk",
-  htmlTemplateEngine: "njk",
-  dataTemplateEngine: "njk",
+  return {
+    templateFormats: ["njk", "html", "md"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
 };
